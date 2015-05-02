01d9e0c8b7ed1298a7d2305fdb650e41ec8c7d8d
Sam Knight <sknight1@sheffield.ac.uk>
2015-05-02 20:13:06 +0100

    Added grass floors and a huge gold floor for testing

--- src/version_7/GameClass.java
+++ src/version_7/GameClass.java
@@ -67,11 +67,11 @@ public class GameClass {
 		
 		//Construct 2 rooms and a corridor to connect them
 		Room room = new Room(9, 11, 11, 29, tiles.get("Gold 6 Floor"), tiles.get("Gold 6 Wall"));
-		Room bgroom = new Room(50, 20, 0, 40, tiles.get("biggold Floor"), tiles.get("biggold Wall"));
+		Room bgroom = new Room(50, 20, 0, 40, tiles.get("hugegold Floor"), tiles.get("biggold Wall"));
 		Room room2 = new Room(7, 8, 1, 21, tiles.get("Tin 6 Floor"), tiles.get("Tin 6 Wall"));
 		Room room3 = new Room(12, 8, 7, 12, tiles.get("Bronze 6 Floor"), tiles.get("Bronze 6 Wall"));
 		Room room4 = new Room(7, 8, 17, 20, tiles.get("Iron 6 Floor"), tiles.get("Iron 6 Wall"));
-		Room room5 = new Room(24, 12, 0, 0, tiles.get("Grass 2 Floor"), tiles.get("Grass 2 Wall"));
+		Room room5 = new Room(24, 12, 0, 0, tiles.get("biggrass Floor"), tiles.get("biggrass Wall"));
 		
 		ArrayList<Entrance> entrances = new ArrayList<Entrance>();
 		entrances.add(new Entrance(Direction.SOUTH, new Coord2D(4, 3), new Coord2D(7, 3)));
